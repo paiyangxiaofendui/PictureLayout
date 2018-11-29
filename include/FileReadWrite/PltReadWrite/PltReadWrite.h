fac1ff3f6827de05ce7b94bfb15b1aa842d3aab8
yuanzibu <459044859@qq.com>
2018-11-30 00:01:01 +0800

    增加头文件

---
+++ include/FileReadWrite/PltReadWrite/PltReadWrite.h
@@ -0,0 +1,26 @@
+/*--------------------------------------------------------------------------------------------------------------------*/
+//	PltReadWrite.h      -- plt文件读写类声明文件
+//	
+//	作者：	yuanzb
+//	时间：	2017.7.21
+//	备注：	
+//
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef		_PltReadWrite_H_
+#define		_PltReadWrite_H_
+
+
+using namespace std ;
+class Panel;
+
+class AFX_EXT_CLASS PltReadWrite
+{
+public:
+	static bool OutputPlt(Panel* pPanel, string strPltFilePath);	
+
+};
+
+
+
+#endif
